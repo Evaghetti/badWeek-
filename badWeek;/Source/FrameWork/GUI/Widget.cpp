894949b2e1d370ae6ecef5becfdf86d93fac4fb0
Enzo Vaghetti <1evaghetti@gmail.com>
2017-12-09 21:40:12 -0200

    Classe base para GUI criada(pode mudar com o tempo)

---
+++ badWeek;/Source/FrameWork/GUI/Widget.cpp
@@ -0,0 +1,3 @@
+#include "Widget.h"
+
+Widget::Widget(const sf::Vector2f & position) : position(position) {}
---
+++ badWeek;/Source/FrameWork/GUI/Widget.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <SFML/Graphics/RenderTarget.hpp>
+#include <SFML/System/Vector2.hpp>
+
+class Widget {
+	public:
+		Widget(const sf::Vector2f& position);
+
+		virtual void draw(sf::RenderTarget& target) = 0;
+
+		virtual void handleInput() = 0;
+		virtual void handleMouse(const sf::Vector2f& mousePos) = 0;
+	protected:
+		virtual bool mouseOver(const sf::Vector2f& mousePos) const = 0;
+	protected:
+		sf::Vector2f position;
+};
